3877e17ad41175de84c67b22c634081b1bca6ab9
Miguel <miguelbritos91@gmail.com>
2021-08-03 00:17:49 -0300

    Se generan rutas de api

--- dev-api/routes/api.php
+++ dev-api/routes/api.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\BreweryController;
 
 /*
 |--------------------------------------------------------------------------
@@ -17,3 +18,8 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+
+Route::get('/brewery', [BreweryController::class, 'index']);
+Route::get('/brewery/{id}', [BreweryController::class, 'show']);
+Route::get('/brewery-filter', [BreweryController::class, 'filter']);
