9c07df743ffd17a9a6366cf66fda6ce2967e835f
Miguel <miguelbritos91@gmail.com>
2021-08-03 01:10:54 -0300

    Se genera frontend layout

---
+++ dev-frontend/components/filter-brewery.vue
@@ -0,0 +1,90 @@
+<template>
+  <div>
+    <form @submit.prevent="onSubmit()">
+      <v-container>
+        <v-row>
+          <v-col
+            cols="12"
+            md="4"
+          >
+            <v-text-field
+              v-model="name"
+              label="Name"
+            />
+          </v-col>
+          <v-col
+            cols="12"
+            md="4"
+          >
+            <v-select
+              v-model="type"
+              :items="items"
+              label="Type"
+            />
+          </v-col>
+          <v-col
+            cols="12"
+            md="4"
+            class="d-flex align-center justify-space-between"
+          >
+            <v-btn
+              class="w-50"
+              color="primary"
+              type="submit"
+            >
+              filter
+            </v-btn>
+            <v-btn
+              class="w-50"
+              color="error"
+              @click="clear"
+            >
+              clear
+            </v-btn>
+          </v-col>
+        </v-row>
+      </v-container>
+    </form>
+  </div>
+</template>
+
+<script>
+
+export default {
+  data: () => ({
+    name: '',
+    type: null,
+    items: [
+      'micro',
+      'nano',
+      'regional',
+      'brewpub',
+      'large',
+      'planning',
+      'bar',
+      'contract',
+      'proprietor',
+      'closed'
+    ]
+  }),
+  methods: {
+    clear () {
+      this.name = ''
+      this.type = null
+    },
+    onSubmit () {
+      const filterData = {
+        name: this.name,
+        type: this.type
+      }
+      this.$emit('filter', filterData)
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+.w-50{
+  width: calc(50% - 10px);
+}
+</style>
---
+++ dev-frontend/pages/index.vue
@@ -0,0 +1,38 @@
+<template>
+  <div>
+    <h1>Brewary List</h1>
+    <filterBrewery @filter="filterBrewery($event)" />
+    <v-data-table
+      :headers="headers"
+      :items="desserts"
+      :items-per-page="5"
+      class="elevation-1"
+    />
+  </div>
+</template>
+
+<script>
+import FilterBrewery from '@/components/filter-brewery'
+export default {
+  components: {
+    FilterBrewery
+  },
+  data: () => ({
+    headers: [
+      { text: 'ID', value: 'id' },
+      { text: 'Name', align: 'start', value: 'name' },
+      { text: 'Type', value: 'type' }
+    ],
+    desserts: []
+  }),
+  methods: {
+    filterBrewery (event) {
+      console.log(event)
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
