81b98e5b3311dd44c1bfe78f788c5f3a83280333
Miguel <miguelbritos91@gmail.com>
2021-08-03 02:17:48 -0300

    Se genera carga de datos desde api y se agrega funcion de filtro por nombre y tipo

--- dev-frontend/pages/index.vue
+++ dev-frontend/pages/index.vue
@@ -2,6 +2,11 @@
   <div>
     <h1>Brewary List</h1>
     <filterBrewery @filter="filterBrewery($event)" />
+    <v-progress-linear
+      v-if="load"
+      indeterminate
+      color="green"
+    />
     <v-data-table
       :headers="headers"
       :items="desserts"
@@ -23,11 +28,49 @@ export default {
       { text: 'Name', align: 'start', value: 'name' },
       { text: 'Type', value: 'type' }
     ],
-    desserts: []
+    desserts: [],
+    load: true
   }),
+  mounted () {
+    this.getBrewaries()
+  },
   methods: {
+    async getBrewaries () {
+      this.load = true
+      const { data } = await this.$axios.get('/api/brewery', {
+        headers: {
+          'Content-Type': 'application/json'
+        }
+      })
+      this.loadBreweries(data)
+    },
+    async getBrewariesByFilter (filters) {
+      this.load = true
+      const { data } = await this.$axios.get('/api/brewery-filter', {
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        params: {
+          name: filters.name,
+          type: filters.type
+        }
+      })
+      this.loadBreweries(data)
+    },
+    loadBreweries (data) {
+      this.desserts = []
+      for (const el of data) {
+        const item = {
+          id: el.id,
+          name: el.name,
+          type: el.brewery_type
+        }
+        this.desserts.push(item)
+      }
+      this.load = false
+    },
     filterBrewery (event) {
-      console.log(event)
+      this.getBrewariesByFilter(event)
     }
   }
 }
